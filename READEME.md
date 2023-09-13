365d23c036998d232ff8f354ed1ac67fc96075fc
jwywoo <jwywoo26@gmail.com>
2023-09-13 13:56:20 +0900

    First Commit: Project Initialized

---
+++ READEME.md
@@ -0,0 +1,5 @@
+# Post Security Api
+
+## Members
+1. 김용글
+2. 정우용
---
+++ src/test/java/com/example/postsecurityapi/PostSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postsecurityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSecurityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
